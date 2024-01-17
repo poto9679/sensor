c333bceddf38793050e11ce45f87ec5a969412dc
poto9679 <81408855+poto9679@users.noreply.github.com>
2024-01-17 08:40:11 +0000

    3day

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Main.css';
 
-function Main() {
-    return (
-        <div>
-            <h1>Welcome to the Main Page</h1>
-            {/* Main 페이지의 콘텐츠 */}
+const Main = () => {
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return(
+    <div className="main-container">
+      <div className="header-box">
+        <div className="title-box">
+          <h1>대시보드 제목</h1>
         </div>
-    );
+        <div className="time-box">
+          <p>{currentTime}</p>
+        </div>
+      </div>
+      {/*여기에 다른 컴포넌트나 내용추가 가능*/}
+    </div>
+  )
 }
+
 
 export default Main;
